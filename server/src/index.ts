36a5998aba29d46ac27cd1411ce9f34a1815d1ad
kristiqn-iliev <kristiqniliev57@gmail.com>
2025-05-06 02:14:29 +0300

    Add knexfile.ts; Make first migration for users table creation.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
-import { config } from "./config";
 import Knex from "knex";
+import { config } from "./config";
+
 const knex = Knex({
   client: "pg",
   connection: config.get("db"),
 });
 
 async function main() {
-  const a = await knex.select().table("books");
+  const a = await knex.select().table("users");
   console.log(a);
+  knex.destroy();
 }
 
 main();
