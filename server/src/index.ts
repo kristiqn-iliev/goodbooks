f94727e658fd657a7050ef03c8fe831acd2e6d7b
kristiqn-iliev <kristiqniliev57@gmail.com>
2025-05-06 00:59:46 +0300

    Set up config variable that establishes the connection to the database; Init env.example file.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,35 @@
-console.log("hello world");
-console.log("meow");
+import dotenvx from "@dotenvx/dotenvx";
+import convict from "convict";
+dotenvx.config();
+
+const config = convict({
+  db: {
+    host: {
+      format: String,
+      default: "localhost",
+      env: "DB_HOST",
+    },
+    port: {
+      format: "port",
+      default: 5432,
+      env: "DB_PORT",
+    },
+    database: {
+      format: String,
+      default: "books",
+      env: "DB_NAME",
+    },
+    user: {
+      format: String,
+      default: "",
+      env: "DB_USER",
+    },
+    password: {
+      format: String,
+      default: "",
+      env: "DB_PASS",
+    },
+  },
+});
+
+console.log(config);
