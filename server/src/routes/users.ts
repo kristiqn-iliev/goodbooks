103443e52f8138151f599f9d2cdc691dc507e81e
kristiqn-iliev <kristiqniliev57@gmail.com>
2025-05-10 13:34:11 +0300

    Create routes folder where rouging logic for express methods will be positioned; Implement some working examples in routes/users.ts file.

---
+++ server/src/routes/users.ts
@@ -0,0 +1,42 @@
+import { json, Router } from "express";
+import { UserService } from "../services/user-service";
+
+export const usersRouter = Router();
+
+//can paste some middleware that will happen
+//for only users queries
+
+const userService = new UserService();
+
+usersRouter.use(json());
+
+usersRouter.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const id = Number(userId);
+  if (Number.isNaN(id)) {
+    res.status(400).json({ error: "Bad request" });
+    return;
+  }
+  const user = await userService.findById(Number(id));
+  if (!user) {
+    res.status(404).json({ error: `User with id ${id} not found!` });
+    return;
+  }
+  res.json({ user: user });
+});
+
+usersRouter.post("/", async (req, res) => {
+  const { email, username, password } = req.body;
+  if (
+    typeof email !== "string" ||
+    typeof username !== "string" ||
+    typeof password !== "string"
+  ) {
+    res.status(400).json({ error: "Bad request" });
+    return;
+  }
+
+  const user = await userService.register({ email, username, password });
+
+  res.json({ user: user });
+});
