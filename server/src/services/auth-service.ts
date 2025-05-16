5e60eb729aa904f14b404d0181dc659c7f44dc3a
kristiqn-iliev <kristiqniliev57@gmail.com>
2025-05-17 02:34:36 +0300

    Adapt services to zod error handling.

--- server/src/services/auth-service.ts
+++ server/src/services/auth-service.ts
@@ -2,13 +2,28 @@ import { User } from "../models/user";
 import { UserService } from "./user-service";
 import bcrypt from "bcrypt";
 
-type CreateUserProps = Pick<User, "email" | "username" | "password">;
-type LoginProps = Pick<User, "email" | "password">;
+import { z } from "zod";
+
+const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
+
+export const registerUserDataSchema = z.object({
+  email: z.string(),
+  username: z.string(),
+  password: z.string().regex(passwordRegex),
+});
+
+export const loginUserDataSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
+
+type RegisterUserData = z.infer<typeof registerUserDataSchema>;
+type LoginUserData = z.infer<typeof loginUserDataSchema>;
 
 export class AuthService {
   private userService = new UserService();
 
-  async register({ email, username, password }: CreateUserProps) {
+  async register({ email, username, password }: RegisterUserData) {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     let user = await this.userService.findByEmail(email);
@@ -31,7 +46,7 @@ export class AuthService {
     return this.userService.toUserEntry(user);
   }
 
-  async login({ email, password }: LoginProps) {
+  async login({ email, password }: LoginUserData) {
     const user = await this.userService.findByEmail(email);
     if (!user) {
       throw new Error("No such user has been registered!");
